9323c6114c12d9f634bdcd4b39d230d2e5d22cbc
suap-dev <suap.dev@gmail.com>
2024-03-10 17:13:37 +0100

    cleanup app initialization

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,8 @@ mod components;
 mod systems;
 
 use bevy::{
-    app::{App, PluginGroup, Startup, Update},
-    diagnostic::FrameTimeDiagnosticsPlugin,
-    utils::default,
-    window::{EnabledButtons, Window, WindowPlugin, WindowResolution},
-    DefaultPlugins,
+    prelude::*,
+    window::{EnabledButtons, WindowResolution},
 };
 
 use crate::systems::{camera, enemies, player};
@@ -18,9 +15,9 @@ const LOGICAL_HEIGHT: f32 = 600.0;
 fn main() {
     let mut app = App::new();
 
-    #[cfg(debug_assertions)] // debug/dev builds only
+    #[cfg(debug_assertions)]
     {
-        use bevy::diagnostic::LogDiagnosticsPlugin;
+        use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
         app.add_plugins((LogDiagnosticsPlugin::default(), FrameTimeDiagnosticsPlugin));
     }
 
@@ -39,7 +36,6 @@ fn main() {
     .add_systems(Startup, camera::setup)
     .add_systems(Startup, player::spawn)
     .add_systems(Startup, enemies::spawn)
-    .add_systems(Update, enemies::update);
-
-    app.run();
+    .add_systems(Update, enemies::update)
+    .run();
 }
