5801134a93abe8247eeb857a7534739943f5e013
suap-dev <suap.dev@gmail.com>
2024-03-10 17:09:56 +0100

    cleanup app initialisation

--- src/main.rs
+++ src/main.rs
@@ -18,30 +18,28 @@ const LOGICAL_HEIGHT: f32 = 600.0;
 fn main() {
     let mut app = App::new();
 
-    app.add_plugins((
-        DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                resolution: WindowResolution::new(LOGICAL_WIDTH, LOGICAL_HEIGHT),
-                resizable: false,
-                enabled_buttons: EnabledButtons {
-                    maximize: false,
-                    ..default()
-                },
-                ..default()
-            }),
-            ..default()
-        }),
-        FrameTimeDiagnosticsPlugin,
-    ));
-
-    app.add_systems(Startup, (camera::setup, enemies::spawn, player::spawn))
-        .add_systems(Update, enemies::update);
-
     #[cfg(debug_assertions)] // debug/dev builds only
     {
         use bevy::diagnostic::LogDiagnosticsPlugin;
-        app.add_plugins(LogDiagnosticsPlugin::default());
+        app.add_plugins((LogDiagnosticsPlugin::default(), FrameTimeDiagnosticsPlugin));
     }
+
+    app.add_plugins(DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(Window {
+            resolution: WindowResolution::new(LOGICAL_WIDTH, LOGICAL_HEIGHT),
+            resizable: false,
+            enabled_buttons: EnabledButtons {
+                maximize: false,
+                ..default()
+            },
+            ..default()
+        }),
+        ..default()
+    }))
+    .add_systems(Startup, camera::setup)
+    .add_systems(Startup, player::spawn)
+    .add_systems(Startup, enemies::spawn)
+    .add_systems(Update, enemies::update);
 
     app.run();
 }
