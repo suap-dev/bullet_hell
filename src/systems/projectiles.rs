29482320903a525de244bfbf4b994325ae1befc7
suap-dev <suap.dev@gmail.com>
2025-03-07 15:05:33 +0100

    style: remove old function, commented out

--- src/systems/projectiles.rs
+++ src/systems/projectiles.rs
@@ -28,29 +28,3 @@ pub fn hit_and_damage_enemy(
         }
     }
 }
-
-// pub fn deal_damage(
-//     mut commands: Commands,
-//     projectiles: Query<
-//         (Entity, &Transform, &attributes::Radius, &attributes::Damage),
-//         With<markers::Projectile>,
-//     >,
-//     mut enemies: Query<
-//         (&Transform, &attributes::Radius, &mut attributes::HitPoints),
-//         With<markers::Enemy>,
-//     >,
-// ) {
-//     for (p_entity, p_transform, p_circumradius, damage) in &projectiles {
-//         for (e_transform, e_circumradius, mut hitpoints) in &mut enemies {
-//             if commands.get_entity(p_entity).is_some()
-//                 && p_transform
-//                     .translation
-//                     .distance_squared(e_transform.translation)
-//                     < (p_circumradius.0 + e_circumradius.0).powi(2)
-//             {
-//                 hitpoints.0 -= damage.0;
-//                 commands.entity(p_entity).try_despawn();
-//             }
-//         }
-//     }
-// }
